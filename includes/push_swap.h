6c24b2acee480696e247b4bc2e83b09d3d9ef6bd
aweaver <aweaver@student.42.fr>
2022-03-28 18:42:16 +0200

    ADD SEVERAL PROTOTYPES & BETTER LAYOUT:
    Adding a better presentation for the whole .h file provinding increased
    readability
    Also adding several functions (ft_pile_size, ft_sort_three, ft_the_algo,
    ft_check_sort)

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 13:20:59 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/18 16:59:28 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/03/28 18:15:44 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,29 @@ typedef struct s_ps_pile
 	struct s_ps_pile	*next;
 }				t_ps_pile;
 
+void		ft_get_index(t_ps_pile *pile_a, int argc);
+void		ft_print_pile(t_ps_pile *pile_a, t_ps_pile *pile_b);
+int			ft_check_sort(t_ps_pile *pile);
+void		ft_sort_three(t_ps_pile **pile);
+void		ft_the_algo(t_ps_pile **pile_a, t_ps_pile **pile_b);
+
+/****************************************************************************/
+/*								PILE FUNCT									*/
+/****************************************************************************/
 t_ps_pile	*ft_last_pile(t_ps_pile *pile);
 t_ps_pile	*ft_ante_pile(t_ps_pile	*pile);
 t_ps_pile	*ft_new_pile(int value);
-void		ft_pile_add_back(t_ps_pile **start, t_ps_pile *new);
 t_ps_pile	*ft_get_pile(int argc, char **argv);
-void		ft_get_index(t_ps_pile *pile_a, int argc);
+int			ft_pile_size(t_ps_pile *pile_a);
+void		ft_pile_add_back(t_ps_pile **start, t_ps_pile *new);
+
+/****************************************************************************/
+/*								OPERATIONS									*/
+/****************************************************************************/
+
 void		ft_push(t_ps_pile **dest, t_ps_pile **srcs);
-void		ft_print_pile(t_ps_pile *pile_a, t_ps_pile *pile_b);
 void		ft_swap_pile(t_ps_pile *pile);
 void		ft_rotate(t_ps_pile **pile);
 void		ft_reverse_rotate(t_ps_pile **pile);
+
 #endif
