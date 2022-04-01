81f19d4d771efe2744ea23cfae7e09538f57cd87
aweaver <aweaver@student.42.fr>
2022-04-01 16:49:57 +0200

    ADD FT_GET_SMALLEST_POS && FT_GET_AIM_A_HIGHEST:
    Added two new functions the former is useful to return the position of
    the smallest index in one given list.
    The latter is usefull to fix an incorrect value given by get_aim_a
    function when the value couldn't find any higher comparison.
    If this case is encountered, the function will behave as such:
    the position the value will aim at is to be right before the smallest
    index of the current list.

--- sources/ft_get_pos.c
+++ sources/ft_get_pos.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 15:32:35 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/01 10:29:13 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/04/01 16:42:36 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,28 @@ void	ft_get_pos(t_ps_pile **pile)
 	}
 }
 
+int	ft_get_smallest_pos(t_ps_pile **pile)
+{
+	t_ps_pile	*tmp;
+	int			smallest;
+	int			smallest_pos;
+
+	tmp = *pile;
+	smallest = INT_MAX;
+	ft_get_pos(pile);
+	smallest_pos = tmp->pos;
+	while (tmp)
+	{
+		if (tmp->index < smallest)
+		{
+			smallest = tmp->index;
+			smallest_pos = tmp->pos;
+		}
+		tmp = tmp->next;
+	}
+	return (smallest_pos);
+}
+
 void	ft_get_cost(t_ps_pile **pile_a, t_ps_pile **pile_b)
 {
 	t_ps_pile	*tmp_a;
@@ -40,14 +62,35 @@ void	ft_get_cost(t_ps_pile **pile_a, t_ps_pile **pile_b)
 	pa_size = ft_pile_size(tmp_a);
 	while (tmp_b)
 	{
-		tmp_b->cost_b = tmp_b->pos + 1;
+		tmp_b->cost_b = tmp_b->pos;
 		if (tmp_b->pos > (pb_size / 2))
-			tmp_b->cost_b = - (pb_size - tmp_b->pos + 1);
+			tmp_b->cost_b = - (pb_size - tmp_b->pos);
 		tmp_b->cost_a = tmp_b->aim_a;
 		if (tmp_b->aim_a > (pa_size / 2))
-			tmp_b->cost_a = - (pa_size - tmp_b->aim_a + 1);
+			tmp_b->cost_a = - (pa_size - tmp_b->aim_a);
 		tmp_b = tmp_b->next;
 	}
+}
+
+int	ft_get_aim_a_highest(t_ps_pile **pile_a, t_ps_pile *tmp, int cmp, int aim)
+{
+	t_ps_pile	*tmp_a;
+	int			cmp_a;
+
+	tmp_a = *pile_a;
+	if (cmp == INT_MAX)
+	{
+		while (tmp_a)
+		{
+			if (tmp->index < cmp_a)
+			{
+				cmp_a = tmp->index;
+				aim = tmp_a->pos;
+			}
+			tmp_a = tmp_a->next;
+		}
+	}
+	return (aim);
 }
 
 void	ft_get_aim_a(t_ps_pile **pile_a, t_ps_pile **pile_b)
@@ -57,7 +100,6 @@ void	ft_get_aim_a(t_ps_pile **pile_a, t_ps_pile **pile_b)
 	int			cmp_a;
 	int			index_aim;
 
-	cmp_a = INT_MAX;
 	tmp_b = (*pile_b);
 	ft_get_pos(pile_a);
 	ft_get_pos(pile_b);
@@ -74,6 +116,7 @@ void	ft_get_aim_a(t_ps_pile **pile_a, t_ps_pile **pile_b)
 			}
 			tmp_a = tmp_a->next;
 		}
+		index_aim = ft_get_aim_a_highest(pile_a, tmp_b, cmp_a, index_aim);
 		tmp_b->aim_a = index_aim;
 		tmp_b = tmp_b->next;
 	}
