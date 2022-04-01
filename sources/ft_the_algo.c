1b79f5f146ab4bae302a84f2f2cef411f7d7db76
aweaver <aweaver@student.42.fr>
2022-04-01 16:47:39 +0200

    MAJOR IMPROVEMENT ON ALGO:
    Fix algorithm tweeking values, improving how the algo behaves.
    Push_swap is now working.
    Needs to be tested on edge cases and leakproofing needs to be done.

--- sources/ft_the_algo.c
+++ sources/ft_the_algo.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 15:20:37 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/01 12:15:10 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/04/01 16:43:35 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	ft_push_half(t_ps_pile **pile_a, t_ps_pile **pile_b)
 	ft_push_rest(pile_a, pile_b, list_size, swapped);
 }
 
-void	ft_get_cheapest_b(t_ps_pile **pile_a, t_ps_pile **pile_b)
+void	ft_get_cheapest(t_ps_pile **pile_a, t_ps_pile **pile_b)
 {
 	t_ps_pile	*tmp;
 	int			cmp;
@@ -58,7 +58,7 @@ void	ft_get_cheapest_b(t_ps_pile **pile_a, t_ps_pile **pile_b)
 	cmp = INT_MAX;
 	while (tmp)
 	{
-		if (ft_abs(tmp->cost_b) + ft_abs(tmp->cost_a) < ft_abs(cmp))
+		if (ft_abs(tmp->cost_a) + ft_abs(tmp->cost_b) < ft_abs(cmp))
 		{
 			cmp = ft_abs(tmp->cost_b) + ft_abs(tmp->cost_a);
 			cost_a = tmp->cost_a;
@@ -69,8 +69,45 @@ void	ft_get_cheapest_b(t_ps_pile **pile_a, t_ps_pile **pile_b)
 	ft_make_choice(pile_a, pile_b, cost_a, cost_b);
 }
 
+void	ft_order_list(t_ps_pile **pile_a)
+{
+	int	smallest_pos;
+	int	pile_size;
+
+	smallest_pos = ft_get_smallest_pos(pile_a);
+	pile_size = ft_pile_size(*pile_a);
+	if (smallest_pos > (pile_size / 2))
+	{
+		while (smallest_pos < pile_size)
+		{
+			smallest_pos++;
+			ft_rra(pile_a);
+		}
+	}
+	else
+	{
+		while (smallest_pos > 0)
+		{
+			smallest_pos--;
+			ft_ra(pile_a);
+		}
+	}
+}
+
 void	ft_the_algo(t_ps_pile **pile_a, t_ps_pile **pile_b)
 {
 	ft_push_half(pile_a, pile_b);
 	ft_sort_three(pile_a);
+	ft_get_aim_a(pile_a, pile_b);
+	ft_get_cost(pile_a, pile_b);
+	ft_print_pile(*pile_a, *pile_b);
+	while ((*pile_b))
+	{
+		ft_get_aim_a(pile_a, pile_b);
+		ft_get_cost(pile_a, pile_b);
+		ft_get_cheapest(pile_a, pile_b);
+		ft_print_pile(*pile_a, *pile_b);
+	}
+	if (ft_check_sort(*pile_a) == 0)
+		ft_order_list(pile_a);
 }
--- sources/ft_the_algo_part_2.c
+++ sources/ft_the_algo_part_2.c
@@ -6,47 +6,47 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 12:11:30 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/01 12:11:54 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/04/01 16:15:01 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 void	ft_both_neg(t_ps_pile **pile_a, t_ps_pile **pile_b,
-		int cost_a, int cost_b)
+		int *cost_a, int *cost_b)
 {
-	while (cost_a < 0 && cost_b < 0)
+	while (*cost_a < 0 && *cost_b < 0)
 	{
-		cost_a++;
-		cost_b++;
+		(*cost_a)++;
+		(*cost_b)++;
 		ft_rrr(pile_a, pile_b);
 	}
 }
 
 void	ft_both_pos(t_ps_pile **pile_a, t_ps_pile **pile_b,
-		int cost_a, int cost_b)
+		int *cost_a, int *cost_b)
 {
-	while (cost_a > 0 && cost_b > 0)
+	while (*cost_a > 0 && *cost_b > 0)
 	{
-		cost_a--;
-		cost_b--;
+		(*cost_a)--;
+		(*cost_b)--;
 		ft_rr(pile_a, pile_b);
 	}
 }
 
-void	ft_cost_left(t_ps_pile **pile, int cost, char a_or_b)
+void	ft_cost_left(t_ps_pile **pile, int *cost, char a_or_b)
 {
-	while (cost != 0)
+	while (*cost != 0)
 	{
-		if (cost > 0)
+		if (*cost > 0)
 		{
-			cost--;
+			(*cost)--;
 			ft_printf("r%c\n", a_or_b);
 			ft_rotate(pile);
 		}
-		else if (cost < 0)
+		else if (*cost < 0)
 		{
-			cost++;
+			(*cost)++;
 			ft_printf("rr%c\n", a_or_b);
 			ft_reverse_rotate(pile);
 		}
@@ -57,10 +57,10 @@ void	ft_make_choice(t_ps_pile **pile_a, t_ps_pile **pile_b, int cost_a,
 		int cost_b)
 {
 	if (cost_a < 0 && cost_b < 0)
-		ft_both_neg(pile_a, pile_b, cost_a, cost_b);
+		ft_both_neg(pile_a, pile_b, &cost_a, &cost_b);
 	else if (cost_a > 0 && cost_b > 0)
-		ft_both_pos(pile_a, pile_b, cost_a, cost_b);
-	ft_cost_left(pile_a, cost_a, 'a');
-	ft_cost_left(pile_b, cost_b, 'b');
+		ft_both_pos(pile_a, pile_b, &cost_a, &cost_b);
+	ft_cost_left(pile_a, &cost_a, 'a');
+	ft_cost_left(pile_b, &cost_b, 'b');
 	ft_pa(pile_a, pile_b);
 }
