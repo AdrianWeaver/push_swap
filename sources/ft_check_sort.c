cbc857b22404056dd4c3c3e16ff77d50bdf8c01f
aweaver <aweaver@student.42.fr>
2022-04-01 14:27:40 +0200

    FIX FT_CHECK_SORT:
    Improved ft_check_sort, function is now consistent with the rest of the
    working functions, using a temporary list instead of a save.

--- sources/ft_check_sort.c
+++ sources/ft_check_sort.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 10:05:49 by aweaver           #+#    #+#             */
-/*   Updated: 2022/03/28 11:00:59 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/04/01 14:26:03 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 int	ft_check_sort(t_ps_pile *pile)
 {
-	t_ps_pile	*save;
+	t_ps_pile	*tmp;
 
-	save = pile;
-	while (pile->next != NULL)
+	tmp = pile;
+	while (tmp->next != NULL)
 	{
-		if (pile->value > pile->next->value)
-		{
-			pile = save;
+		if (tmp->value > tmp->next->value)
 			return (0);
-		}
-		pile = pile->next;
+		tmp = tmp->next;
 	}
-	pile = save;
 	return (1);
 }
