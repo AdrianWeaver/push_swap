847ab5f4340036748ddb17b236965de357c6f6c8
aweaver <aweaver@student.42.fr>
2022-04-01 20:22:27 +0200

    FIX MAIN.C:
    Fix main.c to manage piles of size 2 and 3 without unnecessary
    operations.

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 13:20:00 by aweaver           #+#    #+#             */
-/*   Updated: 2022/04/01 17:37:47 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/04/01 20:19:04 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,19 +51,23 @@ int	main(int argc, char **argv)
 {
 	t_ps_pile	*pile_a;
 	t_ps_pile	*pile_b;
+	int			pile_size;
 
 	ft_check_args(argc, argv);
 	pile_b = NULL;
 	pile_a = ft_get_pile(argc, argv);
-	if (ft_check_sort(pile_a) == 1)
+	pile_size = ft_pile_size(pile_a);
+	if (pile_size == 3)
+		ft_sort_three(&pile_a);
+	else if (pile_size == 2 && ft_check_sort(pile_a) == 1)
+		ft_sa(&pile_a);
+	else
 	{
-		ft_destroy_pile(pile_a);
-		return (0);
+		ft_get_index(pile_a, argc - 1);
+		ft_the_algo(&pile_a, &pile_b);
+		ft_get_aim_a(&pile_a, &pile_b);
+		ft_get_cost(&pile_a, &pile_b);
 	}
-	ft_get_index(pile_a, argc - 1);
-	ft_the_algo(&pile_a, &pile_b);
-	ft_get_aim_a(&pile_a, &pile_b);
-	ft_get_cost(&pile_a, &pile_b);
 	ft_destroy_pile(pile_a);
 	return (0);
 }
